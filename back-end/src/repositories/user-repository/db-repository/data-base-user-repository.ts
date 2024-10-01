e76f869d68d70cdb180286291e503ab993b6c53e
Isaell Kelson <isaellkelson@gmail.com>
2024-09-30 21:16:38 -0300

    update db repository

--- back-end/src/repositories/user-repository/db-repository/data-base-user-repository.ts
+++ back-end/src/repositories/user-repository/db-repository/data-base-user-repository.ts
@@ -2,8 +2,16 @@ import {UserRepository} from "../user-repository";
 import {User} from "../../../modules/user/entities/user";
 
 export class DataBaseUserRepository implements UserRepository {
-    create(user: User): Promise<void> {
-        return Promise.resolve(undefined);
+    private users: User[] = [];
+
+    async create(user: User): Promise<void> {
+        this.users.push(user);
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+
+        const user = this.users.find(user => user.email === email);
+        return user || null;
     }
 
 }
