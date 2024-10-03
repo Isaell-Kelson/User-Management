566d6fd50ee34e20426c38fddcdb99d860b9c045
Isaell Kelson <isaellkelson@gmail.com>
2024-10-03 07:14:41 -0300

    insert link frontend

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://front-isaell.netlify.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
