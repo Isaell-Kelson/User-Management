b61f91d461c51db7ffd8390a81c344063513e18e
Isaell Kelson <isaellkelson@gmail.com>
2024-10-03 09:19:30 -0300

    add localhost in maints

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://front-isaell.netlify.app',
+    origin: ['https://front-isaell.netlify.app', 'http://localhost:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
